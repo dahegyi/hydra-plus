c1043673e8628abe8d4e1fd5eaa94a08c61388e9
dahegyi <dahegyi@protonmail.com>
2022-12-10 17:32:19 +0100

    defineConfig modified

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  if (command === 'serve') {
-    return {
-      plugins: [vue()]
-    }
-  } else {
-    return {
-      plugins: [vue()],
-      base: "/hydra-plus/"
-    }
-  }
-})
+export default defineConfig({
+  plugins: [vue()],
+  base: "/hydra-plus/",
+});
