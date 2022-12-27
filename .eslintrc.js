c58c2fd236ca952def8b58ca34ac33cf6ccbbc54
dahegyi <dahegyi@protonmail.com>
2022-12-27 21:55:54 +0100

    added eslint+prettier, vuex

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["prettier"],
+  rules: {
+    eqeqeq: "error",
+    "no-console": "warn",
+    "prettier/prettier": "error",
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  ignorePatterns: ["node_modules", "build", "dist", "public"],
+};
