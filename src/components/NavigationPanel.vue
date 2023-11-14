be533f8907f5249eae967ffbd322e0d537dc72f8
dahegyi <dahegyi@proton.me>
2023-11-14 22:43:14 +0100

    unfinished: moved beat counter to NavigationPanel component

--- src/components/NavigationPanel.vue
+++ src/components/NavigationPanel.vue
@@ -11,18 +11,67 @@
       <button class="settings" @click="openSettingsModal">
         synth settings
       </button>
-      <button class="send" @click="send">send</button>
+      <button class="send" @click="handleSend">send</button>
     </div>
   </div>
 </template>
 <script>
 import { mapActions } from "vuex";
 
+// @todo: losing window focus breaks the beat counter
+let beatHappened = false;
+let beatCounter = 0;
+let lastBeatTime = 1; // so that the first beat is always logged
+const beatLogInterval = 4;
+
+// eslint-disable-next-line no-undef
+update = () => {
+  const { bpm, fps, time } = window.hydra;
+  const beatInterval = 60 / bpm;
+  const frameInterval = 1 / fps;
+  const timeSinceLastBeat = time % beatInterval;
+
+  if (timeSinceLastBeat < lastBeatTime && !beatHappened) {
+    beatHappened = true;
+    beatCounter += 1;
+
+    if (beatCounter === beatLogInterval) {
+      beatCounter = 0;
+      // console.warn("beat");
+    }
+  }
+
+  lastBeatTime = timeSinceLastBeat;
+
+  if (timeSinceLastBeat > frameInterval) {
+    beatHappened = false;
+  }
+};
+
 export default {
   emits: ["openAddBlockModal", "openThreeModal", "openSettingsModal"],
 
+  data() {
+    return {
+      waitingForBeat: false,
+    };
+  },
+
   methods: {
     ...mapActions(["send"]),
+
+    handleSend() {
+      if (!this.waitingForBeat) {
+        this.waitingForBeat = true;
+        const checkInterval = setInterval(() => {
+          if (beatCounter === 0) {
+            clearInterval(checkInterval);
+            this.waitingForBeat = false;
+            this.send();
+          }
+        }, 10);
+      }
+    },
 
     openAddBlockModal() {
       this.$emit("openAddBlockModal");
