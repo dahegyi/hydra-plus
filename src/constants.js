6a04a2a7097f6cd546986ef3acac0281d723fb9b
dahegyi <dahegyi@proton.me>
2023-11-14 22:23:37 +0100

    updated constants: removed param names, added param mapping

--- src/constants.js
+++ src/constants.js
@@ -75,144 +75,111 @@ export const TYPE_THREE = "three";
 export const TYPE_SIMPLE = "simple";
 export const TYPE_COMPLEX = "complex";
 
+export const PARAM_MAPPINGS = {
+  noise: ["scale", "offset"],
+  voronoi: ["scale", "speed", "blending"],
+  osc: ["frequency", "sync", "offset"],
+  shape: ["sides", "radius", "smoothing"],
+  gradient: ["speed"],
+  src: ["tex"],
+  solid: ["r", "g", "b", "a"],
+  initCam: ["index"],
+  initImage: ["url"],
+  initVideo: ["url"],
+  initScreen: [],
+  "3D": ["scene"],
+  repeat: ["repeatX", "repeatY", "offsetX", "offsetY"],
+  repeatX: ["reps", "offset"],
+  repeatY: ["reps", "offset"],
+  scroll: ["scrollX", "scrollY", "speedX", "speedY"],
+  scrollX: ["scrollX", "speed"],
+  scrollY: ["scrollY", "speed"],
+  rotate: ["angle", "speed"],
+  scale: ["amount", "xMult", "yMult", "offsetX", "offsetY"],
+  pixelate: ["pixelX", "pixelY"],
+  kaleid: ["nSides"],
+  r: ["scale", "offset"],
+  g: ["scale", "offset"],
+  b: ["scale", "offset"],
+  color: ["r", "g", "b", "a"],
+  saturate: ["amount"],
+  hue: ["hue"],
+  posterize: ["bins", "gamma"],
+  shift: ["r", "g", "b", "a"],
+  invert: ["amount"],
+  contrast: ["amount"],
+  brightness: ["amount"],
+  luma: ["threshold", "tolerance"],
+  thresh: ["threshold", "tolerance"],
+  colorama: ["amount"],
+  modulateRepeat: ["repeatX", "repeatY", "offsetX", "offsetY"],
+  modulateRepeatX: ["reps", "offset"],
+  modulateRepeatY: ["reps", "offset"],
+  modulateKaleid: ["nSides"],
+  modulateScrollX: ["scrollX", "speed"],
+  modulateScrollY: ["scrollY", "speed"],
+  modulate: ["amount"],
+  modulateScale: ["multiple", "offset"],
+  modulatePixelate: ["multiple", "offset"],
+  modulateRotate: ["multiple", "offset"],
+  modulateHue: ["amount"],
+  add: ["amount"],
+  sub: ["amount"],
+  layer: [],
+  blend: ["amount"],
+  mult: ["amount"],
+  diff: [],
+  mask: [],
+};
+
 export const SOURCE_FUNCTIONS = [
   {
     name: "noise",
-    params: [
-      {
-        name: "scale",
-        value: 10,
-      },
-      {
-        name: "offset",
-        value: 0.1,
-      },
-    ],
+    params: [10, 0.01],
   },
   {
     name: "voronoi",
-    params: [
-      {
-        name: "scale",
-        value: 5,
-      },
-      {
-        name: "speed",
-        value: 0.3,
-      },
-      {
-        name: "blending",
-        value: 0.3,
-      },
-    ],
+    params: [5, 0.3, 0.3],
   },
   {
     name: "osc",
-    params: [
-      {
-        name: "frequency",
-        value: 60,
-      },
-      {
-        name: "sync",
-        value: 0.1,
-      },
-      {
-        name: "offset",
-        value: 0,
-      },
-    ],
+    params: [60, 0.1, 0],
   },
   {
     name: "shape",
-    params: [
-      {
-        name: "sides",
-        value: 3,
-      },
-      {
-        name: "radius",
-        value: 0.3,
-      },
-      {
-        name: "smoothing",
-        value: 0.01,
-      },
-    ],
+    params: [3, 0.3, 0.01],
   },
   {
     name: "gradient",
-    params: [
-      {
-        name: "speed",
-        value: 1,
-      },
-    ],
+    params: [1],
   },
   {
     name: "src",
-    params: [
-      {
-        name: "tex",
-        value: "o0",
-      },
-    ],
+    params: ["o0"],
   },
   {
     name: "solid",
-    params: [
-      {
-        name: "r",
-        value: "[1, 0, 0]",
-      },
-      {
-        name: "g",
-        value: "[0, 1, 0]",
-      },
-      {
-        name: "b",
-        value: "[0, 0, 1]",
-      },
-      {
-        name: "a",
-        value: 1,
-      },
-    ],
+    params: ["[1, 0, 0]", "[0, 1, 0]", "[0, 0, 1]", 1],
   },
 ];
 
 export const EXTERNAL_SOURCE_FUNCTIONS = [
   {
     name: "initCam",
-    params: [
-      {
-        name: "index",
-        value: 0,
-      },
-    ],
+    params: [0],
   },
   {
     name: "initImage",
     params: [
-      {
-        name: "url",
-        value:
-          "https://upload.wikimedia.org/wikipedia/commons/2/25/Hydra-Foto.jpg",
-      },
+      "https://upload.wikimedia.org/wikipedia/commons/2/25/Hydra-Foto.jpg",
     ],
   },
   {
     name: "initVideo",
-    params: [
-      {
-        name: "url",
-        value: "https://media.giphy.com/media/AS9LIFttYzkc0/giphy.mp4",
-      },
-    ],
+    params: ["https://media.giphy.com/media/AS9LIFttYzkc0/giphy.mp4"],
   },
   // {
   //   name: "init",
-  //
   // },
   {
     name: "initScreen",
@@ -222,346 +189,113 @@ export const EXTERNAL_SOURCE_FUNCTIONS = [
 export const THREE_FUNCTIONS = [
   {
     name: "3D",
-    params: [
-      {
-        name: "scene",
-        value: 0,
-      },
-    ],
+    params: [0],
   },
 ];
 
 export const GEOMETRY_FUNCTIONS = [
   {
     name: "repeat",
-    params: [
-      {
-        name: "repeatX",
-        value: 3,
-      },
-      {
-        name: "repeatY",
-        value: 3,
-      },
-      {
-        name: "offsetX",
-        value: 0.5,
-      },
-      {
-        name: "offsetY",
-        value: 0,
-      },
-    ],
+    params: [3, 3, 0.5, 0],
+    info: {
+      description: "repeat on x and/or y axis.",
+      code: ".repeat(3,3)",
+    },
   },
   {
     name: "repeatX",
-    params: [
-      {
-        name: "reps",
-        value: 3,
-      },
-      {
-        name: "offset",
-        value: 0.5,
-      },
-    ],
+    params: [3, 0.5],
   },
   {
     name: "repeatY",
-    params: [
-      {
-        name: "reps",
-        value: 3,
-      },
-      {
-        name: "offset",
-        value: 0.5,
-      },
-    ],
+    params: [3, 0.5],
   },
   {
     name: "scroll",
-    params: [
-      {
-        name: "scrollX",
-        value: 0.5,
-      },
-      {
-        name: "scrollY",
-        value: 0.5,
-      },
-      {
-        name: "speedX",
-        value: 0.5,
-      },
-      {
-        name: "speedY",
-        value: 0,
-      },
-    ],
+    params: [0.5, 0.5, 0.5, 0],
   },
   {
     name: "scrollX",
-    params: [
-      {
-        name: "scrollX",
-        value: 0.5,
-      },
-      {
-        name: "speed",
-        value: 0.5,
-      },
-    ],
+    params: [0.5, 0.5],
   },
   {
     name: "scrollY",
-    params: [
-      {
-        name: "scrollY",
-        value: 0.5,
-      },
-      {
-        name: "speed",
-        value: 0.5,
-      },
-    ],
+    params: [0.5, 0.5],
   },
   {
     name: "rotate",
-    params: [
-      {
-        name: "angle",
-        value: 10,
-      },
-      {
-        name: "speed",
-        value: 1,
-      },
-    ],
+    params: [10, 1],
   },
   {
     name: "scale",
-    params: [
-      {
-        name: "amount",
-        value: 1.5,
-      },
-      {
-        name: "xMult",
-        value: 1,
-      },
-      {
-        name: "yMult",
-        value: 1,
-      },
-      {
-        name: "offsetX",
-        value: 0.5,
-      },
-      {
-        name: "offsetY",
-        value: 0.5,
-      },
-    ],
+    params: [1.5, 1, 1, 0.5, 0.5],
   },
   {
     name: "pixelate",
-    params: [
-      {
-        name: "pixelX",
-        value: 20,
-      },
-      {
-        name: "pixelY",
-        value: 20,
-      },
-    ],
+    params: [20, 20],
   },
   {
     name: "kaleid",
-    params: [
-      {
-        name: "nSides",
-        value: 4,
-      },
-    ],
+    params: [4],
   },
 ];
 
 export const COLOR_FUNCTIONS = [
   {
     name: "r",
-    params: [
-      {
-        name: "scale",
-        value: 1,
-      },
-      {
-        name: "offset",
-        value: 0,
-      },
-    ],
+    params: [1, 0],
   },
   {
     name: "g",
-    params: [
-      {
-        name: "scale",
-        value: 1,
-      },
-      {
-        name: "offset",
-        value: 0,
-      },
-    ],
+    params: [1, 0],
   },
   {
     name: "b",
-    params: [
-      {
-        name: "scale",
-        value: 1,
-      },
-      {
-        name: "offset",
-        value: 0,
-      },
-    ],
+    params: [1, 0],
   },
   {
     name: "color",
-    params: [
-      {
-        name: "r",
-        value: 1,
-      },
-      {
-        name: "g",
-        value: 0,
-      },
-      {
-        name: "b",
-        value: 1,
-      },
-      {
-        name: "a",
-        value: 1,
-      },
-    ],
+    params: [1, 0, 1, 1],
   },
   {
     name: "saturate",
-    params: [
-      {
-        name: "amount",
-        value: 2,
-      },
-    ],
+    params: [2],
   },
   {
     name: "hue",
-    params: [
-      {
-        name: "hue",
-        value: 0.4,
-      },
-    ],
+    params: [0.4],
   },
   {
     name: "posterize",
-    params: [
-      {
-        name: "bins",
-        value: 3,
-      },
-      {
-        name: "gamma",
-        value: 0.6,
-      },
-    ],
+    params: [3, 0.6],
   },
   {
     name: "shift",
-    params: [
-      {
-        name: "r",
-        value: 0.5,
-      },
-      {
-        name: "g",
-        value: 0,
-      },
-      {
-        name: "b",
-        value: 0,
-      },
-      {
-        name: "a",
-        value: 1,
-      },
-    ],
+    params: [0.5, 0, 0, 1],
   },
   {
     name: "invert",
-    params: [
-      {
-        name: "amount",
-        value: 1,
-      },
-    ],
+    params: [1],
   },
   {
     name: "contrast",
-    params: [
-      {
-        name: "amount",
-        value: 1.6,
-      },
-    ],
+    params: [1.6],
   },
   {
     name: "brightness",
-    params: [
-      {
-        name: "amount",
-        value: 0.4,
-      },
-    ],
+    params: [0.4],
   },
   {
     name: "luma",
-    params: [
-      {
-        name: "threshold",
-        value: 0.5,
-      },
-      {
-        name: "tolerance",
-        value: 0.1,
-      },
-    ],
+    params: [0.5, 0.1],
   },
   {
     name: "thresh",
-    params: [
-      {
-        name: "threshold",
-        value: 0.5,
-      },
-      {
-        name: "tolerance",
-        value: 0.04,
-      },
-    ],
+    params: [0.5, 0.4],
   },
   {
     name: "colorama",
-    params: [
-      {
-        name: "amount",
-        value: 0.005,
-      },
-    ],
+    params: [0.005],
   },
 ];
 
@@ -599,64 +333,27 @@ export const MODULATE_FUNCTIONS = [
   {
     name: "modulate",
     blocks: [],
-    params: [
-      {
-        name: "amount",
-        value: 0.1,
-      },
-    ],
+    params: [1],
   },
   {
     name: "modulateScale",
     blocks: [],
-    params: [
-      {
-        name: "multiple",
-        value: 1,
-      },
-      {
-        name: "offset",
-        value: 1,
-      },
-    ],
+    params: [1, 1],
   },
   {
     name: "modulatePixelate",
     blocks: [],
-    params: [
-      {
-        name: "multiple",
-        value: 10,
-      },
-      {
-        name: "offset",
-        value: 3,
-      },
-    ],
+    params: [10, 3],
   },
   {
     name: "modulateRotate",
     blocks: [],
-    params: [
-      {
-        name: "multiple",
-        value: 1,
-      },
-      {
-        name: "offset",
-        value: 0,
-      },
-    ],
+    params: [1, 0],
   },
   {
     name: "modulateHue",
     blocks: [],
-    params: [
-      {
-        name: "amount",
-        value: 1,
-      },
-    ],
+    params: [1],
   },
 ];
 
@@ -664,22 +361,12 @@ export const BLEND_FUNCTIONS = [
   {
     name: "add",
     blocks: [],
-    params: [
-      {
-        name: "amount",
-        value: 1,
-      },
-    ],
+    params: [1],
   },
   {
     name: "sub",
     blocks: [],
-    params: [
-      {
-        name: "amount",
-        value: 1,
-      },
-    ],
+    params: [1],
   },
   {
     name: "layer",
@@ -688,22 +375,12 @@ export const BLEND_FUNCTIONS = [
   {
     name: "blend",
     blocks: [],
-    params: [
-      {
-        name: "amount",
-        value: 0.5,
-      },
-    ],
+    params: [0.5],
   },
   {
     name: "mult",
     blocks: [],
-    params: [
-      {
-        name: "amount",
-        value: 1,
-      },
-    ],
+    params: [1],
   },
   {
     name: "diff",
