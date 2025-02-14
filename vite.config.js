5ae6402daafecb2c6d5c71f56e573513fec506bf
dahegyi <dahegyi@proton.me>
2025-02-14 12:02:29 +0100

    added hydration mismatch flag to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(({ mode }) => {
   const config = {
-    define: {},
+    define: {
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+    },
     plugins: [vue()],
     resolve: {
       alias: {
